6edb2755bb038c0a587a0a3ec350887ab6bbdcd4
Asilbek <therealasilbek03@gmail.com>
2025-02-20 16:22:12 +0900

    Testing new Git config

--- app.js
+++ app.js
@@ -1,139 +1,140 @@
 const all = [
-    {
-        id: 1,
-        completed: true,
-        text: 'This is a task'
-    },
-    {
-        id: 2,
-        completed: false,
-        text: 'This is another task'
-    },
-    {
-        id: 3,
-        completed: false,
-        text: 'This is yet another task'
-    },
-    {
-        id: 4,
-        completed: true,
-        text: 'This is yet another task 2'
-    }
+  {
+    id: 1,
+    completed: true,
+    text: "This is a task",
+  },
+  {
+    id: 2,
+    completed: false,
+    text: "This is another task",
+  },
+  {
+    id: 3,
+    completed: false,
+    text: "This is yet another task",
+  },
+  {
+    id: 4,
+    completed: true,
+    text: "This is yet another task 2",
+  },
+  {
+    id: 5,
+    completed: false,
+    text: "This is yet another task 3",
+  },
 ];
 
 const items = document.querySelector(".items");
 
 function displayAllTasks() {
-    items.innerHTML = "";
-    all.forEach((item) => {
-        const li = document.createElement('li');
-        li.classList.add('item');
+  items.innerHTML = "";
+  all.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("item");
 
-        const span = document.createElement('span');
-        span.classList.add('close');
-        span.innerHTML = '&times;';
-        li.appendChild(span);
+    const span = document.createElement("span");
+    span.classList.add("close");
+    span.innerHTML = "&times;";
+    li.appendChild(span);
 
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.checked = item.completed;
-        checkbox.classList.add('checkbox');
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.completed;
+    checkbox.classList.add("checkbox");
 
+    const text = document.createElement("span");
+    text.classList.add("task-text");
+    text.innerText = item.text;
 
-        const text = document.createElement('span');
-        text.classList.add('task-text');
-        text.innerText = item.text;
+    li.appendChild(checkbox);
+    li.appendChild(text);
+    items.appendChild(li);
+    if (item.completed) {
+      li.classList.add("completed");
+    }
 
-        li.appendChild(checkbox);
-        li.appendChild(text);
-        items.appendChild(li);
-        if (item.completed) {
-            li.classList.add('completed');
-        }
-
-        checkbox.addEventListener('change', () => {
-            item.completed = checkbox.checked;
-            li.classList.toggle('completed', item.completed);
-        });
+    checkbox.addEventListener("change", () => {
+      item.completed = checkbox.checked;
+      li.classList.toggle("completed", item.completed);
     });
+  });
 }
 
 const addBtn = document.querySelector(".addBtn");
 const input = document.querySelector("#input");
 
-
 displayAllTasks();
 
 function displayActiveTasks() {
-    const activeTasks = all.filter(task => !task.completed);
-    items.innerHTML = "";
-    activeTasks.forEach((item) => {
-        const li = document.createElement('li');
-        li.classList.add('item');
+  const activeTasks = all.filter((task) => !task.completed);
+  items.innerHTML = "";
+  activeTasks.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("item");
 
-        const span = document.createElement('span');
-        span.classList.add('close');
-        span.innerHTML = '&times;';
-        li.appendChild(span);
+    const span = document.createElement("span");
+    span.classList.add("close");
+    span.innerHTML = "&times;";
+    li.appendChild(span);
 
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.checked = item.completed;
-        checkbox.classList.add('checkbox');
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.completed;
+    checkbox.classList.add("checkbox");
 
+    const text = document.createElement("span");
+    text.classList.add("task-text");
+    text.innerText = item.text;
 
-        const text = document.createElement('span');
-        text.classList.add('task-text');
-        text.innerText = item.text;
+    li.appendChild(checkbox);
+    li.appendChild(text);
+    items.appendChild(li);
+    if (item.completed) {
+      li.classList.add("completed");
+    }
 
-        li.appendChild(checkbox);
-        li.appendChild(text);
-        items.appendChild(li);
-        if (item.completed) {
-            li.classList.add('completed');
-        }
-
-        checkbox.addEventListener('change', () => {
-            item.completed = checkbox.checked;
-            li.classList.toggle('completed', item.completed);
-        });
+    checkbox.addEventListener("change", () => {
+      item.completed = checkbox.checked;
+      li.classList.toggle("completed", item.completed);
     });
+  });
 }
 
-const activeItems = document.querySelector('.active');
-activeItems.addEventListener('click', displayActiveTasks);
+const activeItems = document.querySelector(".active");
+activeItems.addEventListener("click", displayActiveTasks);
 // const placeholderText = document.getElementById('placeholder-text');
 
 // const checkBox = document.querySelector('.checkbox');
 // const taskText = document.querySelector('.task-text');
 
-
 // adding an item to all array
 function addItem() {
-    let inputValue = input.value.trim();
-    if (inputValue === '') return alert('Please enter a task');
+  let inputValue = input.value.trim();
+  if (inputValue === "") return alert("Please enter a task");
 
-    const item = {
-        id: Date.now(),
-        completed: false,
-        text: inputValue
-    };
+  const item = {
+    id: Date.now(),
+    completed: false,
+    text: inputValue,
+  };
 
-    input.value = '';
+  input.value = "";
 
-    all.push(item);
+  all.push(item);
 
-    displayAllTasks();
+  displayAllTasks();
 }
 
 // toggling the now class in nav bar
-const navItems = document.querySelectorAll('nav div');
-navItems.forEach(item => {
-    item.addEventListener('click', function () {
-        navItems.forEach(i => i.classList.remove('now'));
+const navItems = document.querySelectorAll("nav div");
+navItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    navItems.forEach((i) => i.classList.remove("now"));
 
-        this.classList.add('now');
-    });
+    this.classList.add("now");
+  });
 });
 // const li = document.createElement('li');
 // li.classList.add('item');
@@ -156,21 +157,20 @@ navItems.forEach(item => {
 // placeholderText.style.display = 'none';
 // // placeholder()
 
+addBtn.addEventListener("click", addItem);
 
-addBtn.addEventListener('click', addItem);
-
-input.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-        addItem();
-    };
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    addItem();
+  }
 });
 
-items.addEventListener('click', function (e) {
-    if (e.target.classList.contains('close')) {
-        e.target.parentElement.remove();
-        placeholder();
-    }
-})
+items.addEventListener("click", function (e) {
+  if (e.target.classList.contains("close")) {
+    e.target.parentElement.remove();
+    placeholder();
+  }
+});
 
 // function placeholder() {
 //     placeholderText.style.display = items.children.length === 0 ? 'block' : 'none';
